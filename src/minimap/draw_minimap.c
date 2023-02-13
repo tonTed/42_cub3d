5fa5d1c356d321e0c114b214ce327cfb3a0aef47
tonted <yddetco@gmail.com>
2023-02-13 15:12:56 -0500

    :sparkles: Add dot position for plater in minimap

--- src/minimap/draw_minimap.c
+++ src/minimap/draw_minimap.c
@@ -6,6 +6,42 @@
 
 #define BLACK 0xFF000000
 #define WHITE 0xFFFFFFFF
+#define ORANGE 0xFF0080FF
+
+int	mm_get_index_raw(int width_img, t_vectorI pos)
+{
+	int	len_y;
+	int	len_x;
+
+	int	len_line = width_img * 4;
+
+	printf("pos: %d, %d", pos.X, pos.Y);
+
+	len_y = len_line * pos.Y;
+	len_x = pos.X * 4;
+
+	printf("len_y: %d, len_x: %d = %d\n", len_y, len_x, len_y + len_x);
+
+	return (len_y + len_x);
+}
+
+void	draw_dot(int size, int color, mlx_image_t *win, t_vectorD pos)
+{
+	WHOAMI
+
+	int row;
+	int med;
+	int index;
+
+	med = size / 2;
+	row = -med;
+	while (row <= med)
+	{
+		index = mm_get_index_raw(win->width, (t_vectorI){pos.X, pos.Y + row});
+		int_memset(&win->pixels[index + (abs(row % size) - med) * 4], color, size - abs(row % size) * 2);
+		row++;
+	}
+}
 
 int	mm_get_index(int width_img, t_vectorI pos, int row)
 {
@@ -21,7 +57,6 @@ int	mm_get_index(int width_img, t_vectorI pos, int row)
 
 	return (len_y + len_x);
 }
-
 
 void	mm_draw_square(t_vars *vars, t_vectorI pos)
 {
@@ -57,8 +92,14 @@ void	mm_draw_squares(t_vars *vars)
 	}
 }
 
+void	mm_draw_player(t_vars *vars)
+{
+	draw_dot(9, ORANGE, vars->mm.win, vars->p.c);
+}
+
 void	draw_minimap(t_vars *vars)
 {
 	fill_image(vars->mm.win, GREY);
 	mm_draw_squares(vars);
+	mm_draw_player(vars);
 }

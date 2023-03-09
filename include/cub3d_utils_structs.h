381d6b2683a9d2e5293c4706c9915037f9831eff
tonted <yddetco@gmail.com>
2023-03-09 10:44:36 -0500

    :recycle: Refactor Header!

---
+++ include/cub3d_utils_structs.h
@@ -0,0 +1,40 @@
+//
+// Created by Teddy BLANCO on 2023-03-09.
+//
+
+#ifndef CUB3D_UTILS_STRUCTS_H
+# define CUB3D_UTILS_STRUCTS_H
+
+typedef struct s_vectorD {
+	double	X;
+	double 	Y;
+}	t_vectorD;
+
+typedef struct s_vectorI {
+	int32_t	X;
+	int32_t	Y;
+}	t_vectorI;
+
+typedef struct s_size {
+	uint32_t	w;
+	uint32_t 	h;
+}	t_size;
+
+typedef struct s_draw_wall
+{
+	double 	wall_height;
+	double 	wall_top;
+	double 	wall_bottom;
+	int 	wall_x;
+	double	ray_length;
+	double	ray_length_orig;
+	double	ray_angle;
+	double 	step_angle;
+	double	step_y;
+	double	off_y;
+	int 	tex_x;
+	double	tex_y;
+	uint8_t orientation;
+} t_draw_wall;
+
+#endif
